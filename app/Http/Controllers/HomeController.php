74669b5e58048b4acd196d4f16d604471dbef89b
Dirk Tepe <dstepe@gmail.com>
2020-09-28 19:36:39 -0400

    Clean up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\MiamiOH\NumberFactDate;
 use App\MiamiOH\NumberFactFinder;
-use App\MiamiOH\NumberFactInteger;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
