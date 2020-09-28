778c7a10d233cad8dc81be62ac7e7841a354b6f3
Dirk Tepe <dstepe@gmail.com>
2020-09-28 19:36:39 -0400

    Clean up

--- app/Http/Controllers/NumberLookupController.php
+++ app/Http/Controllers/NumberLookupController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\MiamiOH\NumberFactDate;
 use App\MiamiOH\NumberFactFinder;
-use App\MiamiOH\NumberFactInteger;
 use Illuminate\Http\Request;
 
 class NumberLookupController extends Controller
